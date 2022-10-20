3f5c0a26630f2a57854dbb8a8de20640f58b8a44
Artur Passos Moreira <103152018+ArturPassosMoreira@users.noreply.github.com>
2022-10-20 00:52:47 +0000

    Criei variáveis

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 # print() = comando de saida
 print("Alo Mundo!")
 
+# Quando quiser guardar uma String!
+nome = "Artur Moreira"
+# Quando quiser guardar um numero inteiro!
+idade = 18
+# Exibir o nome
+print("Seu nome e ", nome)
+
+
+
+
 
 '''
 comentario em bloco
