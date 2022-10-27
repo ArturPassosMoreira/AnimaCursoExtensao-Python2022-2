8246b0bd8dba04d6b26ae4d412e7ba6a703b35e2
Artur Passos Moreira <103152018+ArturPassosMoreira@users.noreply.github.com>
2022-10-27 01:06:01 +0000

    Ler nome do aluno e sua nota, e dizer qual a sua situação

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ print(f"A minha idade é: {idade} ")
 print("A minha idade é: {}".format(idade))
 '''
 
+
+
+
  # SEGUNDA AULA 26/10/2022
-# comando input(): Quero permitir que o usuario digite algo
+'''# comando input(): Quero permitir que o usuario digite algo
 # o usuario digite algo...
 nome = input("Digite seu nome: ")
 # pede a idade "Qual sua idade?"
@@ -41,8 +44,26 @@ if (idade >= 18 and sexo == "M"):
   print("Você precisa se alistar")
 else:
   print("Você não precisa se alistar") 
+'''
 
 
+
+
+#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
+
+nome = str(input("Digite seu nome: "))
+nota = float(input("Qual sua nota: "))
+
+if(nota == 10):
+  print("Parabéns {}, voce tirou  nota maxima!".format(nome))
+elif(nota < 10 and nota >= 7):
+  print("Você passou acima da media!")
+elif(nota == 6):
+  print("Você passou na média.")
+elif(nota < 6 and nota >= 0):
+  print("Você reprovou. :(")
+elif(nota > 10 or nota < 0):
+  print("Insira um valor entre 0 e 10.")
 
 
 
