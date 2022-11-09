afa1489f5cf378554ef8eb3a7e5e6d95afb8a978
Artur Passos Moreira <103152018+ArturPassosMoreira@users.noreply.github.com>
2022-11-09 23:23:24 +0000

    exercitando while e lista

---
+++ aula309.11.2022.py
@@ -0,0 +1,41 @@
+print("Inicio da aula 3")
+
+
+contador = 1
+#exibir de 1 a 10 
+while(contador <= 10 ):
+   print(contador)
+   contador = contador +1 #contador += 1
+
+
+
+  
+# Laço for
+fruta= "morango"
+print(fruta)
+fruta1 ="morango"
+fruta2 ="laranja"
+fruta3 ="pêra"
+#lista
+frutas = ["morango", "laranja", "pêra"]
+
+#Quero exibir a terceira fruta(pêra)
+
+print(frutas[2])
+
+#Exibir quantas frutas tem na lista
+print(len(frutas)) # length = tamanho
+#quero incluir fruta na lista
+frutas.append("manga")
+
+print(len(frutas))
+print(frutas)
+
+print("Exemplo das frutas com while")
+
+frutas.append("uva")
+i = 0
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
+
