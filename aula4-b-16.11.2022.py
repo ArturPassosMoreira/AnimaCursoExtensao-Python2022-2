07ae6caf69c7cbf37afdaf1d5b23b47cfe3a9350
Artur Passos Moreira <103152018+ArturPassosMoreira@users.noreply.github.com>
2022-11-17 00:25:18 +0000

    Adicionei outro personagem-com INSERT

---
+++ aula4-b-16.11.2022.py
@@ -0,0 +1,13 @@
+#Importar biblioteca
+import sqlite3
+# estabelecer conexão com bancos de dados
+conexao = sqlite3.connect("dc_universe.db")
+#criar um ojeto de tipo cursor
+cursor = conexao.cursor()
+#comando para inserir um personagem
+sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry', 'Herói(na)')"
+#Executar o comando SQl
+print(cursor.execute(sql))
+#Confirmar o INSERT com commit() e fechar o banco
+conexao.commit()
+conexao.close
